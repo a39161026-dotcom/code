84987d71303a88d27ba6ebcadbb96e9ebeba2e35
이성헌 <a39161026@gmail.com>
2026-06-25 10:15:53 +0900

    [Passed] 변수 값 복사하기 3 (100ms, 9MB)

---
+++ trail0/변수 값 복사하기 3/copying-variable-values-3.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 5;
+        int c = 3;
+        System.out.println(a+b);
+System.out.println(b-c);  
+System.out.println(b-c+a);
+
+    }
+}
