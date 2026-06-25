d0796d8f91d538cfc4c6a26e24962d6043d6a5a6
이성헌 <a39161026@gmail.com>
2026-06-25 10:50:22 +0900

    [Passed] 사칙연산 연습 (93ms, 9MB)

---
+++ trail0/사칙연산 연습/practice-basic-arithmetic-operations.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 30;
+        int C = a+b;
+        System.out.println(C/8);
+        int c = a+b;
+        System.out.println(C % 8);
+    }
+}
