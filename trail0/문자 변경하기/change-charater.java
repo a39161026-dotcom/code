67066efddc0e08bd133cdffd4d8c27209a53e310
이성헌 <a39161026@gmail.com>
2026-06-25 10:06:01 +0900

    [Passed] 문자 변경하기 (93ms, 9MB)

---
+++ trail0/문자 변경하기/change-charater.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        char a = 'C';  // 여기 채워보기
+         a = 'T';       // 다시 값 넣기
+    System.out.println('T');  // 출력
+    }
+}
+
