f1fcc31eeda0117ffacb429ee6596d98503491bb
이성헌 <a39161026@gmail.com>
2026-08-01 11:50:24 +0900

    [Passed] 두줄 출력 (90ms, 9MB)

---
+++ trail1/두줄 출력/print-two-sentences-introduce.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello students!");
+        System.out.println("Welcome to LeebrosCode!"); // printin -> println 으로 수정
+    }
+}
